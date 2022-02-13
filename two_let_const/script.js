7755c3aaeb57776af8f89ae19dfabe1912c5f760
TimDnepr <dneprpc777@gmail.com>
2022-02-13 18:17:31 +0200

    second_lesson

---
+++ two_let_const/script.js
@@ -0,0 +1,20 @@
+let userAge;
+userAge = 29;
+
+let userName = 'Tymur';
+let userSurname = 'Bobanov';
+
+let newUserName;
+let shoppingListUser;
+let onlineUserNow;
+let salaryUser;
+let cursorCoordinates;
+
+
+let firstScript;
+firstScript = 'Hello World!';
+console.log(firstScript)
+alert(firstScript)
+
+
+console.log('userAge', userAge)
